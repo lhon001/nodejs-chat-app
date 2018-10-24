5343ff2391d82a9a02c5e79a67436f1b13177014
github username <lhon001@gmail.com>
2018-10-24 17:51:37 -0400

    implemented socket.io

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 let messages = [
-  {name: 'Tim', message: "Hi"},
-  {name: 'Jane', message: "Hello"}
+  {name: 'Tim', message: 'Hi'},
+  {name: 'Jane', message: 'Hello'}
 ]
 
 app.get('/messages', (req, res) => {
   res.send(messages)
 })
 
+app.post('/messages', (req, res) => {
+  // console.log("req.body: ", req.body);
+  messages.push(req.body)
+  io.emit('message', req.body)
+  res.sendStatus(200)
+})
 
-let server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('user connected');
+})
+
+let server = http.listen(3000, () => {
     console.log("server is listening on port:", server.address().port);
 });
